8270aa9a51551879dd49a89a0100a503f0c393e7
Olivier Poulin <456crdv@gmail.com>
2024-09-18 12:44:58 -0400

    Fin musique + render bloom dans camera

---
+++ Quille_Raymond_Olivier/Assets/Sound/Music/JouerMusiqueFin.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JouerMusiqueFin : MonoBehaviour
+{
+    public AudioClip musiqueFin;
+    public AudioClip musiqueAmbiance;
+    bool baisse = true;
+    bool restart;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<AudioSource>().clip = musiqueAmbiance;
+        GetComponent<AudioSource>().Play();
+        InvokeRepeating("ChangeMusique", 5f, 0.25f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        print(GetComponent<AudioSource>().volume); 
+    }
+
+    public void ChangeMusique()
+    {
+        if (baisse == true)
+        {
+            GetComponent<AudioSource>().volume -= 0.05f;
+
+            if (GetComponent<AudioSource>().volume <= 0f)
+            {
+                GetComponent<AudioSource>().volume = 0f;
+            }
+
+            if (GetComponent<AudioSource>().volume == 0f)
+            {
+                baisse = false;
+                GetComponent<AudioSource>().clip = musiqueFin;
+                GetComponent<AudioSource>().Play();
+            }
+            print("On baisse");
+        }
+        else if (baisse == false)
+        {
+            GetComponent<AudioSource>().volume += 0.05f;
+
+            if (GetComponent<AudioSource>().volume >= 1f)
+            {
+                GetComponent<AudioSource>().volume = 1f;
+                CancelInvoke("ChangeMusique"); 
+            }
+            print("On monte");
+        }
+    }
+}
