b273388f905a9520db37d80efabf352157c0f8a6
Olivier Poulin <456crdv@gmail.com>
2024-09-19 09:37:13 -0400

    Ajout TMPRO

---
+++ Quille_Raymond_Olivier/Assets/Scripts/UI/Timer.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Timer : MonoBehaviour
+{
+    float timer = 600f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timer > 0)
+        {
+            timer -= Time.deltaTime;
+        }
+    }
+
+
+}
